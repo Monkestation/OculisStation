656858b296ac1e99479b649a7a953f8deeb0552c
Chen Marisa <18307183+flleeppyy@users.noreply.github.com>
2026-01-14 04:58:55 -0800

    Update tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/iris/ooc.tsx

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/iris/ooc.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/iris/ooc.tsx
@@ -4,6 +4,6 @@ export const oocpronouns: Feature<string> = {
   name: 'OOC pronouns',
   category: 'CHAT',
   description:
-    'Pronouns to show in OOC when someone hovers over your username, Separated by forward slashes. Most common pronouns and neopronouns are accepted with a max of 4 (Staff are exempt from limits but please use it in good faith). Example: "she/it/fae"',
+    'Pronouns to show in OOC when someone hovers over your username, Separated by forward slashes. Most common pronouns and neopronouns are accepted. Custom text can be inserted afterwards. Example: "she/it/fae - my note here!"',
   component: FeatureShortTextInput,
 };
