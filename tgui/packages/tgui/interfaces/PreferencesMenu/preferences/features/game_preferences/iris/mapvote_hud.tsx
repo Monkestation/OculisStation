80a3518b9fa7e15a9cf509144fbfdf0540b75cc6
XeonMations <62395746+XeonMations@users.noreply.github.com>
2026-02-23 06:30:19 +0200

    Revert "[PORT] OOC pronouns  (#13)"
    This reverts commit 2288e24d46f71a2634212aec2b62ecb396afb3d7.

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/iris/mapvote_hud.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/iris/mapvote_hud.tsx
@@ -1,8 +1,8 @@
-import { CheckboxInput, type FeatureToggle } from '../../base';
+import { CheckboxInput, FeatureToggle } from '../../base';
 
 export const mapvote_hud: FeatureToggle = {
   name: 'Show votes as a HUD element',
-  category: 'UI',
+  category: 'OOC',
   description:
     'Show any new votes as a HUD element on the top-right section of the screen',
   component: CheckboxInput,
