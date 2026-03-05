97e8406c77132be60200589ed2cb5ffb4b7d9397
XeonMations <62395746+XeonMations@users.noreply.github.com>
2026-03-05 13:03:18 +0200

    Update RedspaceAnchor.tsx

--- tgui/packages/tgui/interfaces/RedspaceAnchor.tsx
+++ tgui/packages/tgui/interfaces/RedspaceAnchor.tsx
@@ -15,7 +15,7 @@ export const RedspaceAnchor = (props) => {
 const { data } = useBackend();
 const { operational } = data;
 return (
-    <Window width={400} height={155}>
+    <Window width={400} height={180}>
         <Window.Content>
         {!operational && <NoticeBox>No data available</NoticeBox>}
         {!!operational && <RedspaceAnchorContent />}
@@ -26,7 +26,7 @@ return (
 
 const RedspaceAnchorContent = (props) => {
 const { act, data } = useBackend();
-const { breaker, charge_count, charging_state, on, operational } = data;
+const { breaker, charge_count, charging_state, on, operational, violetspace_energy } = data;
 return (
     <Section>
         <LabeledList>
@@ -55,7 +55,29 @@ return (
                 <Box color="bad">Not Charging</Box>
             ))}
             {charging_state === 1 && <Box color="average">Charging</Box>}
-            {charging_state === 2 && <Box color="average">Discharging</Box>}
+            {charging_state === 2 && <Box color="bad">Discharging</Box>}
+        </LabeledList.Item>
+        <LabeledList.Item label="Excess Violetspace Energy">
+            <ProgressBar
+            value={violetspace_energy / 100}
+            ranges={{
+            bad: [0.7, Infinity],
+            average: [0.3, 0.7],
+            good: [-Infinity, 0.3],
+            }}
+            />
+        </LabeledList.Item>
+        <LabeledList.Item label="Discharge Violetspace Energy">
+            <Button.Confirm
+            icon={'fire'}
+            children={"Discharge"}
+            color="bad"
+            confirmColor={"danger"}
+            confirmIcon={'fa-exclamation-triangle'}
+            confirmContent={"Confirm?"}
+            disabled={!operational}
+            onClick={() => act('discharge_violetspace')}
+            />
         </LabeledList.Item>
         </LabeledList>
     </Section>
